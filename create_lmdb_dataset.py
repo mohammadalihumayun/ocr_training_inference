b6674bea3af5c629798b70ff0dee0a99375af221
mohammadalihumayun <60851621+mohammadalihumayun@users.noreply.github.com>
2024-12-10 12:17:50 +0500

    Delete create_lmdb_dataset.py

--- create_lmdb_dataset.py
+++
@@ -1,6 +0,0 @@
-#! git clone https://github.com/mohammadalihumayun/UTRNet-High-Resolution-Urdu-Text-Recognition.git
-#! pip install lmdb
-#! pip3 install fire
-
-# train data
-#! python create_lmdb_dataset.py --inputPath 'rois_path' --gtFile 'ground_truth_file_path' --outputPath 'output_path'
