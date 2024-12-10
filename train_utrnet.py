66419dfed26cf8a06670737fa5e9b2ac895331b0
mohammadalihumayun <60851621+mohammadalihumayun@users.noreply.github.com>
2024-12-10 12:12:21 +0500

    Create train_utrnet.py

---
+++ train_utrnet.py
@@ -0,0 +1,12 @@
+# modified from utrnet by Abdulrahman et. al. disabling data augmentation.
+#! git clone https://github.com/mohammadalihumayun/UTRNet-High-Resolution-Urdu-Text-Recognition.git
+#! pip install lmdb
+#! pip install timm==0.6.12
+# %cd '/content/UTRNet-High-Resolution-Urdu-Text-Recognition/'
+
+
+# training from scratch
+#! python3 train.py --train_data /content/val --valid_data /content/val --freeze_visual 'no' --FeatureExtraction HRNet --SequenceModeling DBiLSTM --Prediction CTC --exp_name UTRNet-Large --num_epochs 2 --batch_size 2
+
+# fine tuning from first trained model (set freeze visual to 'yes' or 'no' to freeze cnn based visual feature extraction)
+#! python3 train.py --train_data /content/train --valid_data /content/val --saved_model 'path/z1_e2e_best_norm_ED.pth' --freeze_visual 'no' --Prediction CTC --exp_name UTRNet-Large --num_epochs 1 --batch_size 6
