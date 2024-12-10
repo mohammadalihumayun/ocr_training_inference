26da4b82ae73e7959541158c46cfe667a796b531
mohammadalihumayun <60851621+mohammadalihumayun@users.noreply.github.com>
2024-12-10 12:14:48 +0500

    Update and rename train_utrnet.py to train_infer_utrnet.py

--- train_infer_utrnet.py
+++ train_infer_utrnet.py
@@ -10,3 +10,8 @@
 
 # fine tuning from first trained model (set freeze visual to 'yes' or 'no' to freeze cnn based visual feature extraction)
 #! python3 train.py --train_data /content/train --valid_data /content/val --saved_model 'path/z1_e2e_best_norm_ED.pth' --freeze_visual 'no' --Prediction CTC --exp_name UTRNet-Large --num_epochs 1 --batch_size 6
+
+# inference to save results reading from a source folder
+
+# change option to images_path for modified read.py to read and compute cer for multiple lines
+#! python3 read_save.py --output_text /content/drive/MyDrive/ocr_urdu/seerat/preds.csv --images_path source_rois_path/rois --FeatureExtraction HRNet --SequenceModeling DBiLSTM --Prediction CTC  --saved_model  path/best_norm_ED_tuned_e2e_p14_p70_ep3.pth
